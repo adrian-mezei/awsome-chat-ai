46396112084e61ccf71ed4510ef5c0a9627c40ab
Adrián Mezei <mezei.adrian@gmail.com>
2023-02-05 20:53:30 +0100

    Initialize AWSome Chat with Terraform

---
+++ README.md
@@ -0,0 +1,30 @@
+[![Terraform](https://img.shields.io/badge/Terraform-1.3.5-%23844fba)](https://www.terraform.io/)
+
+# AWSome chat
+
+This repository contains an example chat application with added AI functionalities using AWS services with the infrastructure implemented in Terraform.
+
+## Install and compile
+
+Install dependencies to the backend, frontend and terraform. Compile and bundle frontend and backend application components.
+
+```bash
+$ npm install
+$ npm run build
+```
+
+## Terraform deployment
+
+Configure your AWS CLI profile, then execute the following commands.
+
+```bash
+$ cd terraform
+$ terraform plan
+$ terraform apply
+```
+
+## Clean up terraform deployment
+
+```bash
+$ terraform destroy
+```
---
+++ terraform/lambda.tf
@@ -0,0 +1,39 @@
+locals {
+  lambda_function_name       = "AWSomeChat"
+  lambda_function_invoke_arn = "arn:aws:apigateway:${data.aws_region.this.name}:lambda:path/2015-03-31/functions/arn:aws:lambda:${data.aws_region.this.name}:${local.account_id}:function:${local.lambda_function_name}/invocations"
+}
+
+data "archive_file" "this" {
+  type        = "zip"
+  source_dir  = "${path.module}/../app/backend/bundle"
+  output_path = "${path.module}/lambda.zip"
+}
+
+resource "aws_lambda_function" "this" {
+  function_name    = local.lambda_function_name
+  role             = aws_iam_role.this.arn
+  filename         = data.archive_file.this.output_path
+  source_code_hash = data.archive_file.this.output_base64sha256
+
+  handler = "index.handler"
+  runtime = "nodejs16.x"
+
+  environment {
+    variables = {
+      APIGW_ENDPOINT = "https://${aws_apigatewayv2_api.this.id}.execute-api.${local.region}.amazonaws.com/prod"
+      TABLE_NAME     = local.dynamodb_table_name
+    }
+  }
+
+  depends_on = [
+    aws_cloudwatch_log_group.this,
+    aws_iam_role_policy_attachment.this
+  ]
+}
+
+resource "aws_lambda_permission" "this" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "${aws_apigatewayv2_api.this.execution_arn}/*/*"
+}
